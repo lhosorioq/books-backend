7f80457d7570da1b069b9ea0c4d99daa29839fd4
Luis Humberto Osorio Quiceno <lhosorio@gmail.com>
2022-11-02 11:13:53 -0500

    Complementando tests

--- tests/Feature/BooksApisTest.php
+++ tests/Feature/BooksApisTest.php
@@ -24,6 +24,77 @@ class BooksApisTest extends TestCase
             [
                 'title' => $books[0]->title
             ]
+        )->assertJsonFragment(
+            [
+                'title' => $books[1]->title
+            ]
+        );
+    }
+
+    /** @test */
+    function can_get_one_book()
+    {
+        $book = Book::factory()->create();
+        // $response = $this->getJson(route('books.show', $book));
+        // $response->assertJsonFragment(
+        //     [
+        //         'title' => $book->title
+        //     ]
+        // );
+        $this->getJson(route('books.show', $book))
+            ->assertJsonFragment(
+                [
+                    'title' => $book->title
+                ]
             );
     }
+
+    /** @test */
+    function can_create_book()
+    {
+        $this->postJson(route('books.store'), [])
+            ->assertJsonValidationErrorFor('title');
+        $this->postJson(route('books.store'), [
+            'title' => 'My new little book'
+            ]
+        )->assertJsonFragment(
+            [
+                'title' => 'My new little book'
+            ]
+        );
+        $this->assertDatabaseHas(
+            'books',
+            [
+                'title' => 'My new little book'
+            ]
+        );
+    }
+
+    /** @test */
+    function can_update_books()
+    {
+        $book = Book::factory()->create();
+        $this->patchJson(route('books.update', $book), [])
+            ->assertJsonValidationErrorFor('title');
+        $this->patchJson(route('books.update', $book), [
+            'title' => 'Edited little book'
+        ])->assertJsonFragment(
+            [
+                'title' => 'Edited little book'
+            ]
+        );
+        $this->assertDatabaseHas('books', [
+            'title' => 'Edited little book'
+        ]);
+    }
+
+    /** @test */
+    function can_delete_books()
+    {
+        $book = Book::factory()->create();
+        $this->deleteJson(route('books.destroy', $book))
+            ->assertNoContent();
+        $this->assertDatabaseCount('books', 0);
+    }
+
 }
